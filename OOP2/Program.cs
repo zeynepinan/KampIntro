f947d122e9d645e20b41b6850481253db4c0300d
Zeynep İnan <zeynp.inan3269@gmail.com>
2021-02-28 17:21:48 +0300

    5. Gün İlk Ara

---
+++ OOP2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Zeynep";
+            musteri1.Soyadi = "İnan";
+            musteri1.TcNo = "2222222222";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "22222";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            //Gerçek Müsteri/ Tüzel Müsteri
+            //SOLID
+
+            //new-> referans no
+            // Musteri classı hem GercekMusteri'nin hem de TuzelMusteri'nin referansını tutabiliyor.
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+        }
+    }
+}
---
+++ OOP2/TuzelMusteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Coorporate
+    //Miras: inheritance: Bir sınıfın başka bir sınıftan özelliklerini(attribute) ve metodlarını miras almasıdır.
+   
+    class TuzelMusteri:Musteri
+    {
+       
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+        
+    }
+}
